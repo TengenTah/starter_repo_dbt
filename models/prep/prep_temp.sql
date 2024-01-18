cf17d7989b4043ae67949cad7078d518d83d8210
Takem <ttakem@hotmail.com>
2024-01-18 18:13:16 +0100

    changes

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -6,6 +6,7 @@ add_weekday AS (
     SELECT *,
     DATE_PART('dow', date) AS weekday,
     DATE_PART('day', date) AS day_num,
+    DATE_PART('week', date) AS week,
     DATE_PART('year', date) AS year
     FROM temp_daily
 )
